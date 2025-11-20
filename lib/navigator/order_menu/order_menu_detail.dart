c0c38237759f31f0d507686097a79ef00b838cdc
Alfred Liusten <alfredliu.development@gmail.com>
2025-11-20 14:57:32 +0700

    Online Coffee Shop part 2

--- lib/navigator/order_menu/order_menu_detail.dart
+++ lib/navigator/order_menu/order_menu_detail.dart
@@ -10,6 +10,11 @@ class OrderMenuDetail extends StatefulWidget {
 }
 
 class _OrderMenuDetailState extends State<OrderMenuDetail> {
+  late String? selectOption = widget.orderMenuList.lengthPrice.values.first.toString();
+  late String? selectSugar = widget.orderMenuList.sugarPercent.values.first.toString();
+
+  late double price = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +29,41 @@ class _OrderMenuDetailState extends State<OrderMenuDetail> {
         ),
       ),
 
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromARGB(255, 252, 227, 116),
+            foregroundColor: Colors.white
+          ),
+
+          child: Text(
+            "Price: $price",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Decol'
+            ),
+          ),
+
+          onPressed: () {},
+        ),
+      ),
+
       body: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 15),
             Center(
               child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 32),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.black38,
                       blurRadius: 10,
-                      offset: Offset(13, 10)
+                      offset: Offset(13, 15)
                     )
                   ]
                 ),
@@ -48,18 +75,109 @@ class _OrderMenuDetailState extends State<OrderMenuDetail> {
               ),
             ),
 
-            SizedBox(height: 20),
+            SizedBox(height: 30),
             Text(
               widget.orderMenuList.name,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontFamily: 'Decol'
+                fontFamily: 'Decol',
+                fontSize: 26
               ),
-            )
+            ),
+
+            SizedBox(height: 25),
+            chooseLengthPriceRadio(),
+
+            SizedBox(height: 30),
+            chooseSugarPriceRadio()
           ],
         ),
       ),
+    );
+  }
+
+  Widget chooseLengthPriceRadio() {
+    return Column(
+      children: widget.orderMenuList.lengthPrice.entries.map((list) {
+        return ListTile(
+          title: Row(
+            children: [
+              Text(
+                "${list.key}:",
+                style: TextStyle(
+                  fontFamily: 'Decol',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18
+                ),
+              ),
+
+              SizedBox(width: 20),
+              Text(
+                "Rp. ${list.value}K",
+                style: TextStyle(
+                  fontFamily: 'HarunoUmi',
+                  color: Color.fromARGB(255, 205, 175, 39),
+                  fontSize: 18
+                ),
+              )
+            ],
+          ),
+
+          leading: Radio(
+            value: list.value.toString(),
+            groupValue: selectOption,
+            onChanged: (value) {
+              setState(() {
+                selectOption = value.toString();
+                price = list.value;
+              });
+            },
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget chooseSugarPriceRadio() {
+    return Column(
+      children: widget.orderMenuList.sugarPercent.entries.map((list) {
+        return ListTile(
+          title: Row(
+            children: [
+              Text(
+                "${list.key}:",
+                style: TextStyle(
+                    fontFamily: 'Decol',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                ),
+              ),
+
+              SizedBox(width: 20),
+              Text(
+                "Rp. ${list.value}K",
+                style: TextStyle(
+                    fontFamily: 'HarunoUmi',
+                    color: Color.fromARGB(255, 205, 175, 39),
+                    fontSize: 18
+                ),
+              )
+            ],
+          ),
+
+          leading: Radio(
+            value: list.value.toString(),
+            groupValue: selectSugar,
+            onChanged: (value) {
+              setState(() {
+                selectSugar = value.toString();
+                price = list.value;
+              });
+            },
+          ),
+        );
+      }).toList(),
     );
   }
 }
