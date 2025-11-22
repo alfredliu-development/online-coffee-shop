eab3c79f3bc912b0dda3ddcc47264c6b80545eaa
Alfred Liusten <alfredliu.development@gmail.com>
2025-11-22 20:51:35 +0700

    Online Coffe Shop part 3

--- lib/navigator/account_bar.dart
+++ lib/navigator/account_bar.dart
@@ -28,59 +28,135 @@ class _AccountBarState extends State<AccountBar> {
         ),
       ),
 
-      body: Column(
-        children: [
-          SizedBox(height: 12),
-          Row(
-            children: [
-              Text(
-                "Name",
-                style: TextStyle(
-                  fontFamily: 'HarunoUmi',
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 26),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.person),
+                iconColor: Colors.black87,
+                hintText: "Your Name"
+              ),
+            ),
+
+            SizedBox(height: 25),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                iconColor:Colors.black87,
+                hintText: 'yourEmail@gmail.com',
+              ),
+            
+              keyboardType: TextInputType.emailAddress,
+              validator: (v) {
+                return v!.contains('@') || !v.contains(".com") ? "This email isn't valid" : null;
+              },
+            ),
+
+            SizedBox(height: 25),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: !_passwordVisible,
+              decoration: InputDecoration(
+                icon: Icon(Icons.password),
+                iconColor:Colors.black87,
+                hintText: 'your password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off
+                  ),
+
+                  onPressed: () => setState(() {
+                    _passwordVisible = !_passwordVisible;
+                  }),
+                )
               ),
 
-              SizedBox(width: 10),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  iconColor: Colors.white60,
-                  hintText: "Your Name"
+              keyboardType: TextInputType.visiblePassword,
+            ),
+
+            SizedBox(height: 25),
+            TextFormField(
+              controller: _passwordConfirmController,
+              obscureText: !_passwordConfirmVisible,
+              decoration: InputDecoration(
+                icon: Icon(Icons.password),
+                iconColor:Colors.black87,
+                hintText: 'your password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _passwordConfirmVisible ? Icons.visibility : Icons.visibility_off
+                  ),
+
+                  onPressed: () => setState(() {
+                    _passwordConfirmVisible = !_passwordConfirmVisible;
+                  }),
+                )
+              ),
+
+              keyboardType: TextInputType.visiblePassword,
+            ),
+
+            SizedBox(height: 35),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 252, 227, 116),
+                foregroundColor: Colors.white
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.all(13),
+                child: Text(
+                  "Edit",
+                  style: TextStyle(
+                    fontFamily: 'Decol',
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ],
-          ),
-
-          SizedBox(height: 25),
-          Row(
-            children: [
-              Text(
-                "Email",
-                style: TextStyle(
-                  fontFamily: 'HarunoUmi',
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.email),
-                  iconColor:Colors.white60,
-                  hintText: 'yourEmail@gmail.com',
-                ),
-
-                keyboardType: TextInputType.emailAddress,
-                validator: (v) {
-                  return v!.contains('@') || !v.contains(".com") ? "This email isn't valid" : null;
-                },
-              ),
-            ],
-          )
-        ],
+            
+              onPressed: () => _submitForm(),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _passwordConfirmController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          padding: EdgeInsets.all(20),
+          shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20)
+          ),
+
+          content: Text(
+            'Successful',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Decol'
+            ),
+          ),
+
+          duration: Duration(milliseconds: 3000),
+        ),
+      );
+    }
   }
 }
--- lib/navigator/payment_bar.dart
+++ lib/navigator/payment_bar.dart
@@ -7,6 +7,7 @@ class PaymentBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           "Your Payment",
           style: TextStyle(
@@ -16,7 +17,32 @@ class PaymentBar extends StatelessWidget {
         ),
       ),
 
-      body: Column(),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              "Your Payment",
+              style: TextStyle(
+                fontFamily: 'HarunoUmi',
+                fontWeight: FontWeight.bold,
+                fontSize: 35
+              ),
+            ),
+
+            SizedBox(height: 22),
+            Text(
+              "Rp 0.000",
+              style: TextStyle(
+                fontFamily: 'HarunoUmi',
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontSize: 22
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
